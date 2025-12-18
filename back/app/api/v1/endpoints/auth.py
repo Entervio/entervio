e68fa4b78661c91bfc07dfa698514265acbf7725
JamalEddineEb <jamal-eddine.el-betioui@etu.unice.fr>
2025-12-18 21:56:04 +0100

    no horizontal screen on phone mode #69

--- back/app/api/v1/endpoints/auth.py
+++ back/app/api/v1/endpoints/auth.py
@@ -54,7 +54,6 @@ async def signup(payload: SignupRequest, db: DbSession):
                 json={
                     "email": payload.email,
                     "password": payload.password,
-                    "email_confirm": True,
                     "user_metadata": {
                         "first_name": payload.first_name,
                         "last_name": payload.last_name,
